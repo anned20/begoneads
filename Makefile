7cdb8f6f697b3f25a2d20cd1053f72550d4bbc82
Anne Douwe Bouma <annedouwebouma@gmail.com>
2019-06-09 17:21:29 +0200

    Make publishing to PyPI possible

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
-.PHONY: format
+.PHONY: format publish
 
 format: *.py
 	autopep8 --in-place *.py
+
+publish-pypi:
+	rm -rf dist/*
+	python3 setup.py sdist bdist_wheel
+	python3 -m twine upload dist/*
+
+publish: publish-pypi
--- setup.py
+++ setup.py
@@ -1,33 +1,37 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='begoneads',
-      version='0.0.3',
-      description='BeGoneAds puts some popular hosts file lists into the hosts file as a adblocker measure.',
-      long_description=readme(),
-      url='http://github.com/anned20/begoneads',
-      entry_points = {
-          'console_scripts': ['begoneads=begoneads.begoneads:cli'],
-      },
-      classifiers=[
+
+setup(
+    name='begoneads',
+    version='0.0.4',
+    description='BeGoneAds puts some popular hosts file lists into the hosts file as a adblocker measure.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='http://github.com/anned20/begoneads',
+    entry_points={
+        'console_scripts': ['begoneads=begoneads.begoneads:cli'],
+    },
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: System :: Networking',
-      ],
-      author='Anne Douwe Bouma',
-      author_email='annedouwe@bouma.tech',
-      license='MIT',
-      packages=['begoneads'],
-      install_requires=[
-          'click',
-          'requests',
-          'tqdm',
-      ],
-      zip_safe=False
+    ],
+    author='Anne Douwe Bouma',
+    author_email='annedouwe@bouma.tech',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'requests',
+        'tqdm',
+    ],
+    zip_safe=False
 )
