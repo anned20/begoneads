8dfda377e4dc410e628c411a4dccd37ac98acc5b
Anne Douwe Bouma <annedouwe@bouma.tech>
2019-06-15 11:56:45 +0200

    Update python_requires in setup.py, fix hostsmanager_test and remove requirements.txt

--- begoneads/hostsmanager_test.py
+++ begoneads/hostsmanager_test.py
@@ -19,35 +19,35 @@ with_begoneads = f'''
 def test_has_begoneads():
     with patch('builtins.open', mock_open(read_data=without_begoneads)) as mock_file:
         hosts_manager = HostsManager('/etc/hosts')
-        mock_file.assert_called_with('/etc/hosts', 'r')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
 
         assert hosts_manager.has_begoneads() == None
 
     with patch('builtins.open', mock_open(read_data=with_begoneads)) as mock_file:
         hosts_manager = HostsManager('/etc/hosts')
-        mock_file.assert_called_with('/etc/hosts', 'r')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
 
-        assert str(hosts_manager.has_begoneads()).startswith('<re.Match')
+        assert hosts_manager.has_begoneads()
 
 
 def test_apply_hosts():
     with patch('builtins.open', mock_open(read_data=without_begoneads)) as mock_file:
         hosts_manager = HostsManager('/etc/hosts')
-        mock_file.assert_called_with('/etc/hosts', 'r')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
 
         assert hosts_manager.has_begoneads() == None
 
         hosts_manager.apply_hosts('0.0.0.0 some.test')
 
-        assert str(hosts_manager.has_begoneads()).startswith('<re.Match')
+        assert hosts_manager.has_begoneads()
 
 
 def test_remove_begoneads():
     with patch('builtins.open', mock_open(read_data=with_begoneads)) as mock_file:
         hosts_manager = HostsManager('/etc/hosts')
-        mock_file.assert_called_with('/etc/hosts', 'r')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
 
-        assert str(hosts_manager.has_begoneads()).startswith('<re.Match')
+        assert hosts_manager.has_begoneads()
 
         hosts_manager.remove_begoneads()
 
--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ def readme():
 
 setup(
     name='begoneads',
-    version='0.0.6',
+    version='0.0.7',
     description='BeGoneAds puts some popular hosts file lists into the hosts file as a adblocker measure.',
+    python_requires='>=3.6',
     long_description=readme(),
     long_description_content_type="text/markdown",
     url='http://github.com/anned20/begoneads',
