cb4a7a283e8b58a1353c41af60646a38be915cfa
cid0rz <cid.kampeador@gmail.com>
2022-12-08 17:26:46 +0100

    restructure of the code

--- begoneads/begoneadsGUI.py
+++ begoneads/begoneadsGUI.py
@@ -4,72 +4,119 @@ from tkinter import ttk
 from begoneads import begoneads
 
 
+class Labelscrolledtw(ttk.LabelFrame):
+    def __init__(self, parent, text='Labelscrolledtw', borderwidth=4, width=300, height=200, columns = [("ID", 20, 'center')], selectmode='none'):
+        super().__init__(parent, text=text, borderwidth=borderwidth, width=width, height=height)
+        self.twscroll = tk.Scrollbar(self)
+        self.twscroll.pack(side='right', fill='y')
+        self.tw = ttk.Treeview(self, columns=[col[0] for col in columns], yscrollcommand=self.twscroll.set,
+                               selectmode=selectmode, displaycolumns=[col[0] for col in columns])
+        self.tw.pack()
+        self.tw.column("#0", width=0, stretch='no')
+        for col in columns:
+            self.tw.column(col[0], width=col[1], anchor=col[2])
+        for col in columns:
+            self.tw.heading(col[0], text=col[0])
+        
+        self.tw.tag_configure('todelete', background='red')
+        self.twscroll.config(command=self.tw.yview)
+        self.tw.bind('<1>', self.on_click)
+        self.tw.bind('<3>', self.on_right_click)
+
+    def on_click(self, event):
+        item = self.tw.identify('item', event.x, event.y)
+        if not self.tw.tag_has('todelete', item):
+            self.tw.selection_toggle(item)
+    def on_right_click(self, event):
+        item = self.tw.identify('item', event.x, event.y)
+        print(item)
+        self.tw.selection_remove(item)
+        if self.tw.tag_has('todelete', item):
+            self.tw.item(str(item), tags=())
+        else:
+            self.tw.item(str(item), tags='todelete')
+        print("tag has: ", self.tw.tag_has('todelete'))
+        
+
 class App (tk.Tk):
 
     def __init__(self):
         super().__init__()
         self.title("Begoneads")
-        self.geometry("600x600")
+        self.geometry("600x650")
         self.resizable(False, False)
         self.style=ttk.Style()
         self.style.map('Treeview', background=[('selected', 'green')])
-        self.host_section = ttk.LabelFrame(self, text='Remote Sources', borderwidth=4, width=300, height=200)
-        self.host_section.grid(padx=10, pady=20, row=0, column=0)
-        self.actions_section = ttk.Labelframe(self, text='Actions', borderwidth=4, width=200, height=200)
-        self.actions_section.grid(row=0, column=1)
-        self.tw_scroll=tk.Scrollbar(self.host_section)
-        self.tw_scroll.pack(side='right', fill='y')
+
+        
+        ################################################
+        #REMOTE SECTION
+        ##############################################
+        self.remote_section = Labelscrolledtw(self, text='Remote Sources', borderwidth=4, width=300, height=200, columns=[("ID", 20, 'center'), ("Address", 300, 'w')])
+        self.remote_section.grid(padx=10, pady=20, row=0, column=0)
+        self.sources = list(enumerate(begoneads.sources))
+        self.local_sources = []
+        #for i in self.sources:
+            #print(i)
+
+        for source in self.sources:
+            self.remote_section.tw.insert(parent='', index=source[0], iid=str(source[0]), values=source )
+        self.remote_section.tw.selection_add([str(i) for i in range(len(self.sources))])
+        self.add_remote_entry=tk.Entry(self, width=45)
+        self.add_remote_entry.grid(row=1, column=0, sticky='ne', padx=(0,10))
+        #make a validation of the remote inserted
+        self.add_remote_btn = tk.Button(self, text="add remote", command=self.add_domain)
+        self.add_remote_btn.grid(row=1, column=1, sticky='nw')
+
+
+        ################################################
+        #LOCAL SECTION
+        ##############################################
+        
+        self.local_section = Labelscrolledtw(self, text='Local Sources', borderwidth=4, width=300, height=200, columns=[("ID", 20, 'center'), ("Path", 300, 'w')])
+        self.local_section.grid(padx=10, pady=20, row=2, column=0)
         self.sources = list(enumerate(begoneads.sources))
         #for i in self.sources:
             #print(i)
-        self.host_tw = ttk.Treeview(self.host_section, columns=["ID", "Address"], yscrollcommand=self.tw_scroll.set, selectmode="none", displaycolumns=["ID", "Address"])
-        self.host_tw.pack()
-        self.host_tw.column("#0", width=0, stretch="no")
-        self.host_tw.column("ID", width=20, anchor="center")
-        self.host_tw.column("Address", width=300, anchor="w")
-        self.host_tw.heading("ID", text="ID")
-        self.host_tw.heading("Address", text="Address")
-        self.host_tw.tag_configure('todelete', background='red')
-        for source in self.sources:
-            self.host_tw.insert(parent='', index=source[0], iid=str(source[0]), values=source )
-        self.host_tw.selection_add([str(i) for i in range(len(self.sources))])
-        self.tw_scroll.config(command=self.host_tw.yview)
-        self.host_tw.bind('<1>', self.on_click)
-        self.host_tw.bind('<3>', self.on_right_click)
-        self.add_host_entry=tk.Entry(self, width=45)
-        self.add_host_entry.pack(side='left', anchor='nw', padx=(10,0))
-        #make a validation of the host inserted
-        self.add_host_btn = tk.Button(self, text="add source", command=self.add_domain)
-        self.add_host_btn.pack(side='left', anchor='nw', padx=5)
-        
-    def on_click(self, event):
-        item = self.host_tw.identify('item', event.x, event.y)
-        if not self.host_tw.tag_has('todelete', item):
-            self.host_tw.selection_toggle(item)
-    def on_right_click(self, event):
-        item = self.host_tw.identify('item', event.x, event.y)
-        print(item)
-        self.host_tw.selection_remove(item)
-        if self.host_tw.tag_has('todelete', item):
-            self.host_tw.item(str(item), tags=())
-        else:
-            self.host_tw.item(str(item), tags='todelete')
-        print("tag has: ", self.host_tw.tag_has('todelete'))
-        
-        
+        self.add_local_entry=tk.Entry(self, width=45)
+        self.add_local_entry.grid(row=3, column=0, sticky='ne', padx=(0,10))
+        #make a validation of the local inserted
+        self.add_local_btn = tk.Button(self, text="add local", command=self.add_path)
+        self.add_local_btn.grid(row=3, column=1, sticky='nw')
+
+
+        ################################################
+        #ACTIONS SECTION
+        ##############################################
+        self.actions_section = ttk.Labelframe(self, text='Actions', borderwidth=4, width=200, height=200)
+        self.actions_section.grid(row=0, column=1)
+        self.play_bttn = tk.Button(self.actions_section, text='play', command=self.get_selection)
+        self.play_bttn.pack()
+       
     def add_domain(self):
-        domain = self.add_host_entry.get()
+        domain = self.add_remote_entry.get()
+        if domain == '':
+            return
         self.sources += [(len(self.sources), domain )]
         source = self.sources[-1]
-        self.host_tw.insert(parent='', index=source[0], iid=str(source[0]), values=source )
-        self.host_tw.selection_toggle(str(source[0]))
+        self.remote_section.tw.insert(parent='', index=source[0], iid=str(source[0]), values=source )
+        self.remote_section.tw.selection_toggle(str(source[0]))
+   
+    def add_path(self):
+        path = self.add_local_entry.get()
+        if path == '':
+            return
+        self.local_sources += [(len(self.local_sources), path )]
+        source = self.local_sources[-1]
+        self.local_section.tw.insert(parent='', index=source[0], iid=str(source[0]), values=source )
+        self.local_section.tw.selection_toggle(str(source[0]))
 
     def get_selection(self):
-        sel=self.host_tw.selection_get()
-        print(sel)
-    
-        
-
+        sel=self.remote_section.tw.selection()
+        sel2=self.local_section.tw.selection()
+        remotes = [self.remote_section.tw.item(i)['values'][1] for i in sel]
+        for remote in remotes:
+            print(remote)
         
 app = App()
 app.mainloop()
